04e43ca96c1c52dc949eb1c9692e7374de6f23e6
Elad <eladagmi24@gmail.com>
2022-05-15 22:41:15 +0300

    Before CodeReview changes

--- screen/webview/securemewebview.js
+++ screen/webview/securemewebview.js
@@ -1,70 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { Linking, StyleSheet, PermissionsAndroid, Platform , Alert} from "react-native";
+import { Linking, StyleSheet, PermissionsAndroid, Platform} from "react-native";
 import { WebView } from 'react-native-webview';
 
-
 export default function App() {
-
-const [hasCameraPermission, setHasCameraPermission] = useState(null);
-const [hasLocationPermission, setHasLocationPermission] = useState(null);
-const [hasReadStoragePermission, setHasReadStoragePermission] = useState(null);
-const [hasWriteStoragePermission, setHasWriteStoragePermission] = useState(null);
-const [hasInternetPermission, setHasInternetPermission] = useState(null);
-const [hasRecordAudioPermission, setHasRecordAudioPermission] = useState(null);
-const [hasModifyAudioPermission, setHasModifyAudioPermission] = useState(null);
-const [hasVideoPermission, setHasVideoPermission] = useState(null);
-const [hasAudioCapturePermission, setHasAudioCapturePermission] = useState(null);
-
 const useMount = func => useEffect(() => func(), []);
 
-          
 
 const useInitialURL = () => {
   const [url, setUrl] = useState(null);
   const [processing, setProcessing] = useState(true);
-
+  // Asking for android permissions
   if(Platform.OS === 'android'){
     useEffect(() => {
         (async () => {
           const { status } = await PermissionsAndroid.PERMISSIONS.CAMERA;
-          setHasCameraPermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.ACCESS_FINE_LOCATION');
-          setHasLocationPermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.READ_EXTERNAL_STORAGE');
-          setHasReadStoragePermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.WRITE_EXTERNAL_STORAGE');
-          setHasWriteStoragePermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.INTERNET');
-          setHasInternetPermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.RECORD_AUDIO');
-          setHasRecordAudioPermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.MODIFY_AUDIO_SETTINGS');
-          setHasModifyAudioPermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.VIDEO_CAPTURE');
-          setHasVideoPermission(status === 'granted');
         })();
         (async () => {
           const { status } = await PermissionsAndroid.request('android.permission.AUDIO_CAPTURE');
-          setHasAudioCapturePermission(status === 'granted');
         })();
-
-
       }, []);
-    
   }
  
   useMount(() => {
@@ -73,33 +48,18 @@ const useInitialURL = () => {
       const initialUrl = await Linking.getInitialURL();
 
       // The setTimeout is just for testing purpose
-      setTimeout(() => {
         setUrl(initialUrl);
-        // setUrl('https://stg.10tix.me/ckhKGwNX5gNGpO6yeXaI');
-
+        // setUrl('https://stg.10tix.me/65rV93Ka8MzG9Xpr4GKs');
         setProcessing(false);
-      }, 1000);
     };
 
     getUrlAsync();
   });
+  // Show url in terminal
   console.log(url)
   return { url, processing };
 };
-
-
-
-
   const { url: initialUrl, processing } = useInitialURL();
-  
-  // if(hasCameraPermission !== 'granted' || hasLocationPermission !== 'granted' || hasReadStoragePermission !== 'granted' || 
-  // hasWriteStoragePermission !== 'granted' || hasInternetPermission !== 'granted' || hasRecordAudioPermission !== 'granted' || 
-  // hasModifyAudioPermission !== 'granted' || hasVideoPermission !== 'granted' || hasAudioCapturePermission !== 'granted') {
-  
-  //   Alert.alert("One of the permissions was denied, can not complete the session")
-  //   return;
-  // }
-  
 
   return (
     <WebView source={{ uri: initialUrl }}
@@ -109,13 +69,10 @@ const useInitialURL = () => {
     javaScriptEnabled={true}
     useWebKit={true}
     allowsInlineMediaPlayback={true}
-
     />
   );
 
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-});
+const styles = StyleSheet.create({});
 
